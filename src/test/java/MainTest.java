0a6e667548562af8e7dccfa576b6aee3727ce4e3
John Mazzitelli <mazz@redhat.com>
2016-03-23 14:42:58 -0400

    make sure values are correct

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,20 +8,25 @@ public class MainTest {
     public void testMyOptionWithValue() throws Exception {
         CommandLine<?> cl = Main.doit("mycmd --myoption value --abc 123");
         assert cl.hasOption("abc") : "Should have abc";
+        assert "123".equals(cl.getOptionValue("abc")) : "bad abc value";
         assert cl.hasOption("myoption") : "Should have myoption";
+        assert "value".equals(cl.getOptionValue("myoption")) : "bad myoption value";
     }
 
     @Test
     public void testMyOptionWithoutValue() throws Exception {
         CommandLine<?> cl = Main.doit("mycmd --myoption --abc 123");
         assert cl.hasOption("abc") : "Should have abc";
+        assert "123".equals(cl.getOptionValue("abc")) : "bad abc value";
         assert cl.hasOption("myoption") : "Should have myoption";
+        assert "".equals(cl.getOptionValue("myoption")) : "bad myoption value";
     }
 
     @Test
     public void testNoMyOption() throws Exception {
         CommandLine<?> cl = Main.doit("mycmd --abc 123");
         assert cl.hasOption("abc") : "Should have abc";
+        assert "123".equals(cl.getOptionValue("abc")) : "bad abc value";
         assert !cl.hasOption("myoption") : "Should not have myoption";
     }
 
@@ -30,7 +35,9 @@ public class MainTest {
     public void testMyOptionAtEndWithValue() throws Exception {
         CommandLine<?> cl = Main.doit("mycmd --abc 123 --myoption value");
         assert cl.hasOption("abc") : "Should have abc";
+        assert "123".equals(cl.getOptionValue("abc")) : "bad abc value";
         assert cl.hasOption("myoption") : "Should have myoption";
+        assert "value".equals(cl.getOptionValue("myoption")) : "bad myoption value";
     }
 
     // specify --myoption at the end of the cmdline
@@ -38,6 +45,8 @@ public class MainTest {
     public void testMyOptionAtEndWithoutValue() throws Exception {
         CommandLine<?> cl = Main.doit("mycmd --abc 123 --myoption");
         assert cl.hasOption("abc") : "Should have abc";
+        assert "123".equals(cl.getOptionValue("abc")) : "bad abc value";
         assert cl.hasOption("myoption") : "Should have myoption";
+        assert "".equals(cl.getOptionValue("myoption")) : "bad myoption value";
     }
 }
